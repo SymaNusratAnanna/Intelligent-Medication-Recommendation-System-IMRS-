1e344f1de2f467bdd590559e3d552bc0f7af5380
syma nusrat ananna <symanusrat158@gmail.com>
2025-12-07 01:13:48 +0800

    remove duration

--- web_app.py
+++ web_app.py
@@ -733,10 +733,7 @@ if selected == "🏠 Dashboard":
                             <strong>🤒 Symptoms Treated:</strong><br>
                             <span style='opacity: 0.9;'>{medicine['for_symptoms']}</span>
                         </div>
-                        <div>
-                            <strong>🤒 Symptoms Treated:</strong><br>
-                            <span style='opacity: 0.9;'>{medicine['duration']}</span>
-                        </div>
+                      
                          <div>
                             <strong>🤒 Symptoms Treated:</strong><br>
                             <span style='opacity: 0.9;'>{medicine['key_info']}</span>
